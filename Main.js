b37f9d0931c6d46c85877990413e421d7025029c
Tomas M <tjmonteiro@mail.austral.edu.ar>
2024-12-17 13:55:04 -0300

    Increible responsive mal pc

--- Main.js
+++ Main.js
@@ -74,3 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('hidden');
+});
